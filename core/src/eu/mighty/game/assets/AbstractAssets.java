cf67be05c6cd860f4a2970c938fe664f3465dfa5
Jorge Munhoz Castanher <punkto@gmail.com>
2017-04-10 23:34:25 +0200

    Update AbstractAssets.java

--- core/src/eu/mighty/game/assets/AbstractAssets.java
+++ core/src/eu/mighty/game/assets/AbstractAssets.java
@@ -8,25 +8,21 @@ import com.badlogic.gdx.math.Rectangle;
 
 public abstract class AbstractAssets {
 
-	
+
 	public abstract TextureRegion getAsset(int asset);
-	
+
 	public abstract Animation getAnimation(int anim);
-	
+
 	public abstract TextureRegion getAnimation(int anim, float time);
-	
+
 	public abstract Rectangle getPolygonon(int assetType, float time);
-	
+
 	public abstract void load();
-	
+
 	
 	//Common methods
 	public static Texture loadTexture(String file)
 	{
 		return new Texture(Gdx.files.internal(file));
 	}
-	
 }
-
-
-
